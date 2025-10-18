8a4e738a4fe46d85443edd062d0d39119a18c641
acev-0007 <aacevedo9216@gmail.com>
2025-10-17 17:31:30 -0700

    hey guys, this is currently getting 17/25, its sthe result function, pls help lolol

--- game_of_nim.py
+++ game_of_nim.py
@@ -6,23 +6,48 @@ class GameOfNim(Game):
     the form of a list of (x, y) positions, and a board, in the form of
     a list with number of objects in each row."""
 
-    def __init__(self, board=[3,1]):
-        raise NotImplementedError
+    def __init__(self, board=[3,1]):  
+        self.board = board
+        moves = []
+        for row, count in enumerate(board):
+            for match in range(1, count+1):
+                moves.append((row, match))
+        self.initial = GameState(to_move='MAX', utility=0, board=board, moves=moves)
 
     def actions(self, state):
         """Legal moves are at least one object, all from the same row."""
         return state.moves
 
     def result(self, state, move):
-        raise NotImplementedError
+        if move not in state.moves:
+            return state
+        row, match=move; board=list(state.board); board[row] = board[row] - match
+        if state.to_move == 'MAX': next_player = 'MIN'
+        else: next_player = 'MAX'
+        moves = [(r, n) for r, count in enumerate(board) for n in range(1, count + 1)]
+
+        new_state = GameState(next_player, 0, board, moves)
+        if self.terminal_test(new_state):
+            utility = self.utility(new_state, state.to_move)
+        else:
+            utility = 0
+
+        return GameState(to_move=next_player, utility=utility, board=board, moves=moves)
+
+         
 
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        raise NotImplementedError
+        if self.terminal_test(state):
+            if state.to_move == player:
+                return -1
+            else : return 1
+        return 0
 
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        raise NotImplementedError
+        return max(state.board) == 0
+        
 
     def display(self, state):
         board = state.board
